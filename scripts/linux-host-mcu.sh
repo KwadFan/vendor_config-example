7c91034c12a7fcba08502f9e4a7c68898678aa37
Stephan Wendel <me@stephanwe.de>
2023-10-23 19:49:54 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- scripts/linux-host-mcu.sh
+++ scripts/linux-host-mcu.sh
@@ -21,6 +21,24 @@ set -Ee
 ## Debug
 # set -x
 
+### Helper funcs
+
+stop_klipper() {
+    if systemctl is-active --quiet klipper.service ;then
+        printf "Trying to stop 'klipper.service' ...\n"
+        sudo systemctl stop klipper.service
+    else
+        printf "Klipper service seems not running, continue...\n"
+    fi
+}
+
+restart_klipper() {
+    if ! systemctl is-active --quiet klipper.service ;then
+        printf "Trying to restart 'klipper.service' ...\n"
+        sudo systemctl start klipper.service
+    fi
+}
+
 ### Ask for sudo!
 printf "Some actions require 'sudo' permissions!\n"
 printf "Please type in your sudo password if asked!\n"
@@ -28,12 +46,7 @@ printf "Please type in your sudo password if asked!\n"
 sudo printf "\n"
 
 ### Stop klipper first!
-if systemctl is-active --quiet klipper.service ;then
-    printf "Trying to stop 'klipper.service' ...\n"
-    sudo systemctl stop klipper.service
-else
-    printf "Klipper service seems not running, continue...\n"
-fi
+stop_klipper
 
 ### Clean up, from previous builds
 if [[ -d ~/klipper ]]; then
@@ -43,21 +56,24 @@ if [[ -d ~/klipper ]]; then
     make distclean
     popd &> /dev/null
 else
-    printf "OOOPS! Something went wrong. Klipper seems not to be installed!"
+    printf "OOOPS! Something went wrong. Klipper seems not to be installed! Exiting ..."
+    restart_klipper
     exit 1
 fi
 
 ### Copy config
-
-echo "$PWD"
-
+printf "Trying to copy linux-host-mcu config file to appropriate location ...\n"
+if [[ -d "${PWD}/firmware_configs/linux-host-mcu" ]]; then
+    cp "${PWD}/firmware_configs/linux-host-mcu/config" ~/klipper/.config
+else
+    printf "OOOPS! Something went wrong. config is missing! Exiting ...\n"
+    restart_klipper
+    exit 1
+fi
 ### build firmware
 
 ### Restart klipper service
-if ! systemctl is-active --quiet klipper.service ;then
-    printf "Trying to restart 'klipper.service' ...\n"
-    sudo systemctl start klipper.service
-fi
+restart_klipper
 
 ### Done message
 printf "Build and Install of linux-host-mcu done!\nGood bye...\n"
